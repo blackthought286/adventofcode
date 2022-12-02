5f10eaa6c5a09cc13bc0b9ebe89e38a8c0cafec6
Everett <blackthought286@gmail.com>
2022-12-02 16:05:53 -0500

    code cleanup

--- index.js
+++ index.js
@@ -14,45 +14,41 @@ const game_object = [
     {name: 'Scissors', opponent: SCISSORS_OPPONENT, user: SCISSORS_US, id: 3}
 ];
 
-const Rock = 1;
-const Paper = 2;
-const Scissors = 3;
-
 try {
     const data = fs.readFileSync('rockpaperscissors.txt', 'utf8').toString().split("\n");
-    //let x = 0;
+
     let total_user_points = 0;
     for (let i in data) {
         let game_value = data[i].split(" ");
         const opponent_item = game_object.find((game) => game.opponent === game_value[0]);
         const user_item = game_object.find((game) => game.user === game_value[1]);
 
-        if (opponent_item.name == user_item.name) {
+        if (opponent_item.name === user_item.name) {
             console.log("this game is a draw");
             total_user_points = total_user_points + 3 + user_item.id;
         }
 
-        if (user_item.id == 1 && opponent_item.id == 3){
+        if (user_item.id === 1 && opponent_item.id === 3){
             total_user_points = total_user_points + user_item.id + 6;
         }
 
-        if (opponent_item.id == 1 && user_item.id == 3){
+        if (opponent_item.id === 1 && user_item.id === 3){
             total_user_points = total_user_points + 0 + user_item.id;
         }
 
-        if (user_item.id == 3 && opponent_item.id == 2){
+        if (user_item.id === 3 && opponent_item.id === 2){
             total_user_points = total_user_points + user_item.id + 6;
         }
 
-        if (opponent_item.id == 3 && user_item.id == 2){
+        if (opponent_item.id === 3 && user_item.id === 2){
             total_user_points = total_user_points + 0 + user_item.id;
         }
 
-        if (user_item.id == 2 && opponent_item.id == 1){
+        if (user_item.id === 2 && opponent_item.id === 1){
             total_user_points = total_user_points + user_item.id + 6;
         }
 
-        if (opponent_item.id == 2 && user_item.id == 1){
+        if (opponent_item.id === 2 && user_item.id === 1){
             total_user_points = total_user_points + 0 + user_item.id;
         }
 
