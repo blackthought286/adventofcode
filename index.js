4d7fee37d72f19199011c4556721302d51a6bd00
Everett <blackthought286@gmail.com>
2022-12-02 16:02:42 -0500

    the correct first part of advent 2nd day

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
 const fs = require('fs');
 
+const ROCK_OPPONENT = "A";
+const PAPER_OPPONENT = "B";
+const SCISSORS_OPPONENT = "C";
+
+const ROCK_US = "X";
+const PAPER_US = "Y";
+const SCISSORS_US = "Z";
+
+const game_object = [
+    {name: 'Rock', opponent: ROCK_OPPONENT, user: ROCK_US, id: 1},
+    {name: 'Paper', opponent: PAPER_OPPONENT, user: PAPER_US, id: 2},
+    {name: 'Scissors', opponent: SCISSORS_OPPONENT, user: SCISSORS_US, id: 3}
+];
+
+const Rock = 1;
+const Paper = 2;
+const Scissors = 3;
+
 try {
-    const data = fs.readFileSync('calories.txt', 'utf8').toString().split("\n");
+    const data = fs.readFileSync('rockpaperscissors.txt', 'utf8').toString().split("\n");
+    //let x = 0;
+    let total_user_points = 0;
+    for (let i in data) {
+        let game_value = data[i].split(" ");
+        const opponent_item = game_object.find((game) => game.opponent === game_value[0]);
+        const user_item = game_object.find((game) => game.user === game_value[1]);
 
-    let temp_var = 0;
-    let arr = new Array();
-
-    for (i in data) {
-        if (data[i] != ""){
-            temp_var += parseInt(data[i]);
-        }else{
-            arr.push(temp_var);
-            temp_var = 0;
+        if (opponent_item.name == user_item.name) {
+            console.log("this game is a draw");
+            total_user_points = total_user_points + 3 + user_item.id;
         }
+
+        if (user_item.id == 1 && opponent_item.id == 3){
+            total_user_points = total_user_points + user_item.id + 6;
+        }
+
+        if (opponent_item.id == 1 && user_item.id == 3){
+            total_user_points = total_user_points + 0 + user_item.id;
+        }
+
+        if (user_item.id == 3 && opponent_item.id == 2){
+            total_user_points = total_user_points + user_item.id + 6;
+        }
+
+        if (opponent_item.id == 3 && user_item.id == 2){
+            total_user_points = total_user_points + 0 + user_item.id;
+        }
+
+        if (user_item.id == 2 && opponent_item.id == 1){
+            total_user_points = total_user_points + user_item.id + 6;
+        }
+
+        if (opponent_item.id == 2 && user_item.id == 1){
+            total_user_points = total_user_points + 0 + user_item.id;
+        }
+
     }
 
+    console.log(total_user_points);
 
-    console.log(Math.max(...arr));
+
 
 } catch (e) {
     console.error(e);
