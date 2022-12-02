5aa8e98da053c8b4d93dac6b70b8d176f7b298d9
Everett <blackthought286@gmail.com>
2022-12-01 22:57:17 -0500

    finished part two of first day of advent

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ try {
     let temp_var = 0;
     let arr = new Array();
 
+    let arr_again = new Array();
+
+    let highest_elves = 3;
+
     for (i in data) {
         if (data[i] != ""){
             temp_var += parseInt(data[i]);
@@ -15,8 +19,16 @@ try {
         }
     }
 
-
     console.log(Math.max(...arr));
+
+    for (let i = 0; i < 3; i++) {
+        let x = Math.max(...arr);
+
+        arr_again.push(x);
+        arr = arr.filter(e => e !== x);
+    }
+    console.log(arr_again);
+    console.log(arr_again.reduce((a, b) => a + b, 0));
 
 } catch (e) {
     console.error(e);
