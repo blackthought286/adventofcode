e33652c752a4d15c45303e0b67a91e1503613047
Everett <blackthought286@gmail.com>
2022-12-06 02:11:18 -0500

    correct answer for advent of code sixth day part one

--- index.js
+++ index.js
@@ -1,58 +1,39 @@
 const fs = require('fs');
 
-let crate1 = ['D', 'M', 'S', 'Z', 'R', 'F', 'W', 'N'];
-let crate2 = ['W', 'P', 'Q', 'G', 'S'];
-let crate3 = ['W', 'R', 'V', 'Q', 'F', 'N', 'J', 'C'];
-let crate4 = ['F', 'Z', 'P', 'C', 'G', 'D', 'L'];
-let crate5 = ['T', 'P', 'S'];
-let crate6 = ['H', 'D', 'F', 'W', 'R', 'L'];
-let crate7 = ['Z', 'N', 'D', 'C'];
-let crate8 = ['W', 'N', 'R', 'F', 'V', 'S', 'J', 'Q'];
-let crate9 = ['R', 'M', 'S', 'G', 'Z', 'W', 'V'];
-
-let create_container = [
-    { id: 1, crate: crate1 },
-    { id: 2, crate: crate2 },
-    { id: 3, crate: crate3 },
-    { id: 4, crate: crate4 },
-    { id: 5, crate: crate5 },
-    { id: 6, crate: crate6 },
-    { id: 7, crate: crate7 },
-    { id: 8, crate: crate8 },
-    { id: 9, crate: crate9 },
-];
-
-let final_arr = [];
-
 try {
-    const data = fs.readFileSync('crates.txt', 'utf8').toString().split("\n");
+    //const data = fs.readFileSync('packet_marker.txt', 'utf8').toString().split("\n");
+    const data = fs.readFileSync('packet_marker.txt', 'utf8').toString();
 
-    let total_score = 0;
+    let total = 0;
+    let total_characters = 0;
+
+    let container = [];
+
+    let findDup = true;
 
     for (let i in data) {
-        //let elf_assignments = data[i].split(/[-,]/);
-        // turn string into numerical values so we can operate. first value in array
-        // is how many to move, second value is which stack to move from and third value
-        // is what stack to move to
-        let elf_assignments = data[i].split(" ").filter(val => !["move", "from", "to"].includes(val));
+        if (container.length >= 4){
+            let x = container.splice(-3);
+            container = x;
+        }
 
-        let container_move_from = create_container.find((container) => container.id === parseInt(elf_assignments[1]));
-        let container_move_to = create_container.find((container) => container.id === parseInt(elf_assignments[2]));
-        //console.log(elf_assignments);
+        container.push(data[i]);
 
-        // move the correct stacks and make sure you reverse stacks coming from first stack
-        let x = container_move_from.crate.splice(-Math.abs(parseInt(elf_assignments[0])));
-        //let b = x.reverse();
-        let y = container_move_to.crate.concat(x);
-        container_move_to.crate = y;
+        findDup = container.every((e, i, a) => a.indexOf(e) === i);
 
-    }
-    for (let i in create_container) {
-        let x = create_container[i].crate.pop();
-        final_arr.push(x);
+        if (findDup === false && container.length >= 4) {
+            total++;
+        }
+
+        total_characters++;
+
+        if (findDup === true && container.length >= 4) {
+            break;
+        }
     }
 
-    console.log(final_arr);
+    console.log(total);
+    console.log(total_characters);
 
 } catch (e) {
     console.error(e);
